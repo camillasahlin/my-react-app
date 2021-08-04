f5654fd4e4ad5dff7577ff399f21a8923f29d4d8
camillasahlin <84519101+camillasahlin@users.noreply.github.com>
2021-08-04 10:45:42 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,14 @@ import React from "react";
 import './App.css';
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return <div className="App">
+    <h1>Weather App</h1>
+    <footer>
+      <a href="https://github.com/camillasahlin/my-react-app"
+    target="_blank"
+    rel="noreferrer">Open-sourced</a> coded by Camilla Sahlin.</footer>
+    
+    </div>;
 }
 
  
