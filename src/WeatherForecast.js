d7e80ed656aae88f678f7a85c7caaa5b73fb9bf1
camillasahlin <84519101+camillasahlin@users.noreply.github.com>
2021-08-18 11:08:18 +0200

    Adding weather icon via API

---
+++ src/WeatherForecast.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+
+export default function WeatherForecast(props) {
+    return (
+    <div className="WeatherForecast">
+        <div className="row">
+            <div className="col">
+
+            </div>
+        </div>
+    </div>
+    );
+}
+
