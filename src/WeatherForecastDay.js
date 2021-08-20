e3fa328553f5430d576d818d6bcaf444f43ad777
camillasahlin <84519101+camillasahlin@users.noreply.github.com>
2021-08-20 13:55:59 +0200

    Amended daily forecast to showing 5 days

--- src/WeatherForecastDay.js
+++ src/WeatherForecastDay.js
@@ -26,7 +26,7 @@ export default function WeatherForecastDay(props) {
       <div>
         <div className="WeatherForecast-day mt-4">{day()}</div>
             <WeatherIcon code={props.data.weather[0].icon} size={36} />
-            <div className="WeatherForecast-temperature">
+            <div className="WeatherForecast-temperatures">
                 <span className="WeatherForecast-temperature-max">{maxTemperature()}</span>
                 <span className="WeatherForecast-temperature-min">{minTemperature()}</span>
             </div>
